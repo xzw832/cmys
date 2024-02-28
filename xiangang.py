5ebe7c01576e758b633329153916f5fe37101044
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-02-29 02:09:07 +0800

    Update xiangang.py

--- xiangang.py
+++ xiangang.py
@@ -41,9 +41,9 @@ def worker():
             ts_url = channel_url_t + ts_lists[0]  # 拼接单个视频片段下载链接
 
             # 多获取的视频数据进行5秒钟限制
-            with eventlet.Timeout(5, False):
+            with eventlet.Timeout(4, False):
                 start_time = time.time()
-                content = requests.get(ts_url).content
+                content = requests.get(ts_url, timeout=4).content
                 end_time = time.time()
                 response_time = (end_time - start_time) * 1
 
