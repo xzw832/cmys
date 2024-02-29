f9feeb00a013c28e08aed76b9d2755cafe5c48c2
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-02-29 20:09:44 +0800

    Update itv.py

--- itv.py
+++ itv.py
@@ -84,7 +84,7 @@ for url in urls:
     driver = webdriver.Chrome(options=chrome_options)
     # 使用WebDriver访问网页
     driver.get(url)  # 将网址替换为你要访问的网页地址
-    time.sleep(3)
+    time.sleep(6)
     # 获取网页内容
     page_content = driver.page_source
 
