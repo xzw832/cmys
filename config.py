baa64a4a62aef7a22c0fb1fb1cc28d077ccd4a08
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-11 00:13:53 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+source_file = "demo.txt"
+final_file = "result.txt"
+favorite_list = [
+    "凤凰",
+    "汕头",
+]
+favorite_page_num = 1
+default_page_num = 1
+urls_limit = 8
+response_time_weight = 0.5
+resolution_weight = 0.5
