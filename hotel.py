0900cb0f37b076ffb5b63b2848458fa48e907d44
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-10 23:42:17 +0800

    Update hotel.py

--- hotel.py
+++ hotel.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import requests
+from bs4 import BeautifulSoup
 import re
 
 
