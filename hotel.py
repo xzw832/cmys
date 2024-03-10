7da053cf06e94eee2b95bbc5d240e9def1f46e38
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-10 18:57:34 +0800

    Create hotel.py

---
+++ hotel.py
@@ -0,0 +1,39 @@
+import time
+import os
+import concurrent.futures
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import requests
+import re
+
+urls = [
+    "http://tonkiang.us/hoteliptv.php?page=1&s=%E5%87%A4%E5%87%B0"    #湖南
+    ]
+
+results = []
+
+for url in urls:
+    # 创建一个Chrome WebDriver实例
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.add_argument("blink-settings=imagesEnabled=false")
+    driver = webdriver.Chrome(options=chrome_options)
+    # 使用WebDriver访问网页
+    driver.get(url)  # 将网址替换为你要访问的网页地址
+    time.sleep(10)
+    # 获取网页内容
+    page_content = driver.page_source
+
+    # 关闭WebDriver
+    driver.quit()
+
+    # 查找所有符合指定格式的网址
+    pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
+    urls_all = re.findall(pattern, page_content)
+    # urls = list(set(urls_all))  # 去重得到唯一的URL列表
+    urls = set(urls_all)  # 去重得到唯一的URL列表
+    for url in urls:
+      print(urls)
