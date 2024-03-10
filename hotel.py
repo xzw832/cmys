8c494e887dad8ff45543b5008e952f6052a9c959
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-11 00:05:19 +0800

    Update hotel.py

--- hotel.py
+++ hotel.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium_stealth import stealth
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 import requests
