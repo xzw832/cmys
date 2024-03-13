a08dcdce098687669298fcc231b08e8c448efd91
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-13 12:43:18 +0800

    Update hotel.py

--- hotel.py
+++ hotel.py
@@ -40,7 +40,7 @@ for url in urls:
     driver = webdriver.Chrome(options=chrome_options)
     # 使用WebDriver访问网页
     driver.get(url)  # 将网址替换为你要访问的网页地址
-    time.sleep(15)
+    time.sleep(10)
     # 获取网页内容
     page_content = driver.page_source
 
@@ -79,12 +79,12 @@ for ipv in sorted_list:
         page_url= f"http://tonkiang.us/9dlist2.php?s={ipv}"
         print(page_url)
         driver.get(page_url)  # 将网址替换为你要访问的网页地址
-        WebDriverWait(driver, 15).until(
+        WebDriverWait(driver, 10).until(
             EC.presence_of_element_located(
                 (By.CSS_SELECTOR, "div.tables")
                 )
         )
-        time.sleep(15)
+        time.sleep(10)
         soup = BeautifulSoup(driver.page_source, "html.parser")
         # 关闭WebDriver
         driver.quit()
