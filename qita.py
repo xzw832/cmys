a68376132e00ffdd2cadddb863c57e9139b9c650
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-14 21:18:04 +0800

    Update qita.py

--- qita.py
+++ qita.py
@@ -84,15 +84,15 @@ def worker():
                 now=time.time()
                 res=se.get(channel_url,headers=headers,timeout=5,stream=True)
                 if res.status_code==200:
-                    for k in res.iter_content(chunk_size=3145728):
+                    for k in res.iter_content(chunk_size=2097152):
                         # 这里的chunk_size是1MB，每次读取1MB测试视频流
                         # 如果能获取视频流，则输出读取的时间以及链接
                         if k:
                             print(f'{time.time()-now:.2f}\t{channel_url}')
                             response_time = (time.time()-now) * 1
-                            download_speed = 3145728 / response_time / 1024
+                            download_speed = 2097152 / response_time / 1024
                             normalized_speed = min(max(download_speed / 1024, 0.001), 100)
-                            if response_time > 2:
+                            if response_time > 1:
                                 result = channel_name, channel_url, f"{normalized_speed:.3f} MB/s"
                                 results.append(result)
                             else:
