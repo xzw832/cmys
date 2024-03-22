fdcd7df371b5f4294753bfd2aeaff29604f016c7
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-23 06:07:35 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,14 +1,14 @@
 import requests
 
-url = 'http://mywlkj.ddns.net:754/tv/1905.php?id=cctv6'  # 替换为你想测试的URL
-response = requests.get(url, allow_redirects=True)
+channel_url = 'http://mywlkj.ddns.net:754/tv/1905.php?id=cctv6'  # 替换为你想测试的URL
+response = requests.get(channel_url, allow_redirects=True)
 
 # 检查是否有重定向发生
 if response.history:
     # 如果有重定向历史，说明发生了重定向
     print("发生了重定向")
     # 获取最终重定向到的地址
-    final_url = response.url
+    final_url = response.channel_url
     print("最终地址:", final_url)
 else:
     # 如果没有重定向历史，说明没有发生重定向
