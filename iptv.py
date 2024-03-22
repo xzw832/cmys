a744b6dfc80e6c76b47648433bd737df02eaf656
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-23 00:37:52 +0800

    Update iptv.py

--- iptv.py
+++ iptv.py
@@ -18,34 +18,7 @@ infoList = []
 urls_y = []
 resultslist = []
 urls = [
-    "http://tonkiang.us/hoteliptv.php?page=1&s=凤凰",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=凤凰",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=揭阳",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=揭阳",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=珠海",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=广州",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=广州",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=汕头",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=汕头",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=深圳",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=深圳",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=广东",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=广东",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=新闻",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=新闻",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=综合",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=综合",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=北京",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=上海",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=香港",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=香港",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=重庆",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=苏州",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=成都",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=杭州",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=厦门",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=厦门",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=美国"
+    "https://raw.githubusercontent.com/taijichadao/tv/main/itv.txt"
     ]
 # 初始化计数器为0
 counter = -1
@@ -83,19 +56,8 @@ for url in urls:
 
     # 关闭WebDriver
     driver.quit()
-    print(increment_counter())    #方便看看是否有执行啊
-    # 查找所有符合指定格式的网址
-    pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
-    urls_all = re.findall(pattern, page_content)
-    # urls = list(set(urls_all))  # 去重得到唯一的URL列表
-    urls_y = set(urls_all)  # 去重得到唯一的URL列表
-    for urlv in urls_y:
-        resultslist.append(f"{urlv}")
+    print(page_content)    #方便看看是否有执行啊
 
-resultslist = set(resultslist)    # 去重得到唯一的URL列表
-
-with open("iplist.txt", 'w', encoding='utf-8') as file:
-    for iplist in resultslist:
-        file.write(iplist + "\n")
-        print(iplist)
-    file.close()
+with open("iptv_all.txt", "w", encoding="utf-8") as output:
+    output.write('\n'.join(page_content))
+    output.close()
