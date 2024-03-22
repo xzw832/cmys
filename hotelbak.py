96f1ba30cd2c9d4c7e4ef09b135ad3868d3a7991
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-23 00:59:16 +0800

    Update hotelbak.py

--- hotelbak.py
+++ hotelbak.py
@@ -1,4 +1,4 @@
-import config
+import random
 import concurrent.futures
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
@@ -9,33 +9,59 @@ import time
 import os
 import re
 from bs4 import BeautifulSoup
-    "http://tonkiang.us/hoteliptv.php?page=1&s=凤凰",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=凤凰",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=揭阳",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=揭阳",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=广州",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=广州",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=汕头",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=汕头",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=汕尾",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=汕尾",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=广东",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=广东"
+from queue import Queue
+import threading
+
+lock = threading.Lock()
 # 查找所有符合指定格式的网址
 infoList = []
 urls_y = []
 resultslist = []
 urls = [
-    "http://tonkiang.us/hoteliptv.php?page=1&s=%E5%87%A4%E5%87%B0",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=%E5%87%A4%E5%87%B0",
-    "http://tonkiang.us/hoteliptv.php?page=3&s=%E5%87%A4%E5%87%B0",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=%E6%8F%AD%E9%98%B3",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=%E5%B9%BF%E5%B7%9E",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=%E6%B1%95%E5%A4%B4",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=%E6%B1%95%E5%B0%BE",
-    "http://tonkiang.us/hoteliptv.php?page=2&s=%E6%B1%95%E5%A4%B4",
-    "http://tonkiang.us/hoteliptv.php?page=1&s=%E7%94%B5%E5%BD%B1"
+    "http://tonkiang.us/hoteliptv.php?page=1&s=凤凰",
+    "http://tonkiang.us/hoteliptv.php?page=2&s=凤凰",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=揭阳",
+    "http://tonkiang.us/hoteliptv.php?page=2&s=揭阳",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=珠海",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=广州",
+    "http://tonkiang.us/hoteliptv.php?page=2&s=广州",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=汕头",
+    "http://tonkiang.us/hoteliptv.php?page=2&s=汕头",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=深圳",
+    "http://tonkiang.us/hoteliptv.php?page=2&s=深圳",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=广东",
+    "http://tonkiang.us/hoteliptv.php?page=2&s=广东",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=新闻",
+    "http://tonkiang.us/hoteliptv.php?page=2&s=新闻",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=综合",
+    "http://tonkiang.us/hoteliptv.php?page=2&s=综合",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=北京",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=上海",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=香港",
+    "http://tonkiang.us/hoteliptv.php?page=2&s=香港",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=重庆",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=苏州",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=成都",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=杭州",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=厦门",
+    "http://tonkiang.us/hoteliptv.php?page=2&s=厦门",
+    "http://tonkiang.us/hoteliptv.php?page=1&s=美国"
     ]
+# 初始化计数器为0
+counter = -1
+ 
+# 每次调用该函数时将计数器加1并返回结果
+def increment_counter():
+    global counter
+    counter += 1
+    return counter
+
+#判断一个数字是单数还是双数可
+def is_odd_or_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
 
 for url in urls:
     # 创建一个Chrome WebDriver实例
@@ -46,15 +72,18 @@ for url in urls:
     chrome_options.add_experimental_option("useAutomationExtension", False)
     chrome_options.add_argument("blink-settings=imagesEnabled=false")
     driver = webdriver.Chrome(options=chrome_options)
+    driver.set_page_load_timeout(60)  # 10秒后超时
+    # 设置脚本执行超时
+    driver.set_script_timeout(50)  # 5秒后超时
     # 使用WebDriver访问网页
     driver.get(url)  # 将网址替换为你要访问的网页地址
-    time.sleep(15)
+    time.sleep(20)
     # 获取网页内容
     page_content = driver.page_source
 
     # 关闭WebDriver
     driver.quit()
-
+    print(increment_counter())    #方便看看是否有执行啊
     # 查找所有符合指定格式的网址
     pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
     urls_all = re.findall(pattern, page_content)
@@ -62,40 +91,49 @@ for url in urls:
     urls_y = set(urls_all)  # 去重得到唯一的URL列表
     for urlv in urls_y:
         resultslist.append(f"{urlv}")
-        
+
+resultslist = set(resultslist)    # 去重得到唯一的URL列表
+
 with open("iplist.txt", 'w', encoding='utf-8') as file:
     for iplist in resultslist:
         file.write(iplist + "\n")
         print(iplist)
     file.close()
+    
+sorted_list = sorted(resultslist)
 
-resultslist = set(resultslist)    # 去重得到唯一的URL列表
-
-for ipv in resultslist:   
+def worker(thread_url,counter_id):
     try:
         # 创建一个Chrome WebDriver实例
         results = []
         chrome_options = Options()
+        chrome_options.add_argument(f"user-data-dir=selenium{counter_id}")
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_experimental_option("useAutomationExtension", False)
         chrome_options.add_argument("blink-settings=imagesEnabled=false")
         driver = webdriver.Chrome(options=chrome_options)
+        # 设置页面加载超时
+        driver.set_page_load_timeout(60)  # 10秒后超时
+     
+        # 设置脚本执行超时
+        driver.set_script_timeout(50)  # 5秒后超时
         # 使用WebDriver访问网页
-        page_url= f"http://foodieguide.com/iptvsearch/alllist.php?s={ipv}"
+        if is_odd_or_even(random.randint(1, 200)):
+            page_url= f"http://tonkiang.us/9dlist2.php?s={thread_url}"
+        else:
+            page_url= f"http://foodieguide.com/iptvsearch/alllist.php?s={thread_url}"
+        
         print(page_url)
         driver.get(page_url)  # 将网址替换为你要访问的网页地址
-        WebDriverWait(driver, 20).until(
+        WebDriverWait(driver, 45).until(
             EC.presence_of_element_located(
                 (By.CSS_SELECTOR, "div.tables")
                 )
         )
-        time.sleep(20)
-
+        time.sleep(1)
         soup = BeautifulSoup(driver.page_source, "html.parser")
-        # 关闭WebDriver
-        driver.quit()
         tables_div = soup.find("div", class_="tables")
         results = (
             tables_div.find_all("div", class_="result")
@@ -105,7 +143,8 @@ for ipv in resultslist:
         if not any(
             result.find("div", class_="m3u8") for result in results
         ):
-            break
+            #break
+            print("Err-------------------------------------------------------------------------------------------------------")
         for result in results:
             #print(result)
             m3u8_div = result.find("div", class_="m3u8")
@@ -114,18 +153,6 @@ for ipv in resultslist:
             m3u8_name_div = result.find("div", class_="channel")
             url_name = m3u8_name_div.text.strip() if m3u8_div else None
             #－－－－－
-            info_div = (
-                m3u8_div.find_next_sibling("div") if m3u8_div else None
-            )
-            resolution = None
-            if info_div:
-                info_text = info_div.text.strip()
-                resolution = (
-                    info_text.partition(" ")[2].partition("•")[2]
-                    if info_text.partition(" ")[2].partition("•")[2]
-                    else None
-                )
-            #infoList.append((url_int, resolution))
             #print("-------------------------------------------------------------------------------------------------------")
             name =f"{url_name}"
             if len(name) == 0:
@@ -133,9 +160,10 @@ for ipv in resultslist:
             #print(name)
             urlsp =f"{url_int}"
             if len(urlsp) == 0:
-                urlsp = "rtp://127.0.0.1"           
+                urlsp = "rtp://127.0.0.1"             
             print(f"{url_name}\t{url_int}")
             #print("-------------------------------------------------------------------------------------------------------")
+            urlsp = urlsp.replace("http://67.211.73.118:9901", "")
             name = name.replace("cctv", "CCTV")
             name = name.replace("中央", "CCTV")
             name = name.replace("央视", "CCTV")
@@ -180,14 +208,31 @@ for ipv in resultslist:
             name = name.replace("CCTV5+体育赛事", "CCTV5+")
             name = name.replace("CCTV5+体育", "CCTV5+")
             name = name.replace("CMIPTV", "")
-            name = name.replace("台", "")
             name = name.replace("内蒙卫视", "内蒙古卫视")
-            infoList.append(f"{name},{urlsp}")
+            name = name.replace("CCTVCCTV", "CCTV")
+            if "http" in urlsp:
+                # 获取锁
+                lock.acquire()
+                infoList.append(f"{name},{urlsp}")
+                # 释放锁
+                lock.release()
+        print(f"=========================>>> Thread {thread_url} save ok")
     except Exception as e:
-        print(f"Error on page {ipv}: {e}")
-        #continue
+        print(f"=========================>>> Thread {thread_url} caught an exception: {e}")
+    finally:
+        # 确保线程结束时关闭WebDriver实例
+        driver.quit() 
+        print(f"=========================>>> Thread {thread_url}  quiting")
+        # 标记任务完成
+        time.sleep(0)
 
-        
+# 创建一个线程池，限制最大线程数为3
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    # 提交任务到线程池，并传入参数
+    counter = increment_counter()
+    for i in sorted_list:  # 假设有5个任务需要执行
+        executor.submit(worker, i ,counter)
+
 infoList = set(infoList)  # 去重得到唯一的URL列表
 infoList = sorted(infoList)
 
