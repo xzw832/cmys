51347ac02f8f540103115b68b377be814d90276b
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-23 12:11:07 +0800

    Update cctv.py

--- cctv.py
+++ cctv.py
@@ -176,19 +176,17 @@ def worker():
             try:
                 channel_url_t = channel_url.rstrip(channel_url.split('/')[-1])  # m3u8链接前缀
                 lines = requests.get(channel_url,headers=headers, timeout=3, stream=True).text.strip().split('\n')  # 获取m3u8文件内容
-                count = lines.count('#')
                 ts_lists = [line.split('/')[-1] for line in lines if line.startswith('#') == False]  # 获取m3u8文件下视频流后缀
                 ts_lists_0 = ts_lists[0].rstrip(ts_lists[0].split('.ts')[-1])  # m3u8链接前缀
                 ts_url = channel_url_t + ts_lists[0]  # 拼接单个视频片段下载链接
+    
                 # 多获取的视频数据进行5秒钟限制
                 with eventlet.Timeout(5, False):
                     start_time = time.time()
                     content = requests.get(ts_url,headers=headers, timeout=(2,5), stream=True).content
                     end_time = time.time()
                     response_time = (end_time - start_time) * 1
-                    if response_time > 10:
-                        print(f'－－－－－－Time out\t{channel_url}')
-                        break
+    
                 if content:
                     with open(ts_lists_0, 'ab') as f:
                         f.write(content)  # 写入文件
@@ -218,8 +216,6 @@ def worker():
                 # 释放锁
                 lock.release()
                 numberx = (len(results) + len(error_channels)) / len(channels) * 100
-                print(f'＝＝＝＝＝＝＝＝＝＝＝＝＝＝Time out\t{channel_url}')
-                break
         else:
             try:
                 now=time.time()
@@ -238,7 +234,7 @@ def worker():
                                 response_time = (time.time()-now) * 1
                                 download_speed = 2097152 / response_time / 1024
                                 normalized_speed = min(max(download_speed / 1024, 0.001), 100)
-                                if response_time > 1.4:
+                                if response_time > 1.2:
                                     result = channel_name, channel_url, f"{normalized_speed:.3f} MB/s"
                                     # 获取锁
                                     lock.acquire()
