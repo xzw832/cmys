cfa76ee99dceab18be4569b069dd403c6ed36ac2
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-25 05:14:17 +0800

    Update tv570.py

--- tv570.py
+++ tv570.py
@@ -164,6 +164,7 @@ def channel_key(channel_name):
         return float('inf')  # 返回一个无穷大的数字作为关键字
 
 # 对频道进行排序
+results = sorted(results)
 results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
 #results.sort(key=lambda x: channel_key(x[0]))
 now_today = datetime.date.today()
