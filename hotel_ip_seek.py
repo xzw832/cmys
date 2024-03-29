71f5eaab48dc73ea5f062fe3885fd79f9d45aa9c
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-29 18:10:43 +0800

    Update hotel_ip_seek.py

--- hotel_ip_seek.py
+++ hotel_ip_seek.py
@@ -314,8 +314,8 @@ with open("seekip_ok.txt", 'w', encoding='utf-8') as file:
         file.write(f"{channel_name},{channel_url}\n")
         new_line = f"{channel_name},{channel_url}"
         replace_line_in_file(file_path, channel_name, new_line)
-    file.write(f"测试完成时间,{now_today}\n")
-    file.close()
+file.write(f"测试完成时间,{now_today}\n")
+file.close()
 
 
 # 使用示例
