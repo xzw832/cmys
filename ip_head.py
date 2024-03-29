e66434b0d658a0bdf7d021147d77dec69642fdf2
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-30 03:50:19 +0800

    Update ip_head.py

--- ip_head.py
+++ ip_head.py
@@ -1,4 +1,5 @@
 import requests
+from requests.exceptions import Timeout
 
 load_urls = [
     "http://mywlkj.ddns.net:754/tv.php",
@@ -23,24 +24,33 @@ def get_redirected_urls(url_list):
             if count == 1:
                 if line:
                     channel_name, channel_url = line.split(',')
-                    if 'http' in channel_url:
-                        response = requests.head(channel_url, allow_redirects=False)
-                        print(response)
-                        # 如果初始请求返回200，但之后服务器又发出了302重定向，我们需要处理这种情况
-                        if response.status_code == 200 and 'Location' in response.headers:
-                            redirected_url = response.headers['Location']
-                            redirected_response = session.head(redirected_url)
-                            new_url = channel_name, redirected_response.url
-                            redirected_urls.append(new_url)
-                        # 如果初始请求直接返回了重定向，我们直接返回重定向的URL
-                        elif response.status_code in [301, 302, 303, 307, 308]:
-                            print("--------------------",response.headers['Location'])
-                            new_url = channel_name, response.headers['Location']
-                            redirected_urls.append(new_url)
-                        else:
-                            # 如果没有重定向，返回原始URL
-                            new_url = channel_name, channel_url
-                            redirected_urls.append(new_url)
+                    print("==========>>>>>",channel_name, channel_url)
+                    if '#' not in channel_url:
+                        try:
+                            response = requests.head(channel_url, allow_redirects=False, timeout=3.0)
+                            # 如果初始请求返回200，但之后服务器又发出了302重定向，我们需要处理这种情况
+                            if response.status_code == 200 and 'Location' in response.headers:
+                                redirected_url = response.headers['Location']
+                                redirected_response = session.head(redirected_url)
+                                new_url = channel_name, redirected_response.url
+                                redirected_urls.append(new_url)
+                            # 如果初始请求直接返回了重定向，我们直接返回重定向的URL
+                            elif response.status_code in [301, 302, 303, 307, 308]:
+                                print("--------------------",response.headers['Location'])
+                                new_url = channel_name, response.headers['Location']
+                                redirected_urls.append(new_url)
+                            else:
+                                # 如果没有重定向，返回原始URL
+                                new_url = channel_name, channel_url
+                                redirected_urls.append(new_url)
+                        except Timeout:
+                            print("请求超时")
+                        except requests.RequestException as e:
+                            print(f"请求发生错误: {e}")
+                    else:
+                        # 如果没有重定向，返回原始URL
+                        new_url = channel_name, channel_url
+                        redirected_urls.append(new_url)
                 else:
                     redirected_urls.append(line)
             else:
