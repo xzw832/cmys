7607dbb1ee3d9ef46ffe0cd8f951cd58bf52cace
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-03-31 21:25:44 +0800

    Update ip_qianxin_check.py

--- ip_qianxin_check.py
+++ ip_qianxin_check.py
@@ -68,8 +68,7 @@ def get_redirected_urls(url_list):
                             new_url = f"error_{channel_name}", channel_url
                             redirected_urls.append(new_url)
                             print(f"请求发生错误: {e}")
-        except:
-            print(line)
+
         if increment_counter() > 10:
             print(f"执行完成，次数: {counter}")
             break
