d4fbda366abbc030295f5334401064e624ca5ad6
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-04-02 00:54:50 +0800

    Update url_ffmpeg.py

--- url_ffmpeg.py
+++ url_ffmpeg.py
@@ -1,17 +1,19 @@
 import ffmpeg
 
 def get_live_source_resolution(url):
-    # 使用ffmpeg.input来获取输入流
+    # 使用ffmpeg.input获取输入流
     input_stream = ffmpeg.input(url)
     
-    # 使用ffmpeg.output来设置输出参数，这里我们不真正输出到文件，而是使用null输出
+    # 使用ffmpeg.output设置输出参数，这里我们不真正输出到文件，而是使用null输出
+    # 注意：新版本ffmpeg-python可能不再需要.global_args()
     output_stream = ffmpeg.output(input_stream, 'null', format='null', vframes=1, vf='scale=n:-1')
     
-    # 运行ffmpeg命令并捕获其输出
-    ffmpeg_output = output_stream.run_capture()
+    # 执行FFmpeg命令并获取输出
+    ffmpeg_process = output_stream.run()
+    ffmpeg_output = ffmpeg_process.stdout.decode()
     
     # 在输出中查找分辨率信息
-    for line in ffmpeg_output.stdout.decode().split('\n'):
+    for line in ffmpeg_output.split('\n'):
         if 'Stream' in line and 'Video' in line:
             resolution = line.split(',')[0].split(' ')[-1]
             return resolution.split('x')
