b859fd9868924b2467dcd5140bb78d3732876af2
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-04-02 08:56:27 +0800

    Update nanxun_tv.py

--- nanxun_tv.py
+++ nanxun_tv.py
@@ -5,11 +5,14 @@ from requests.exceptions import Timeout
 import chardet
 
 load_urls = [
-    "http://tvbox.nx66.bf:99/tvbox/zhibo.php"
+    "http://tvbox.nx66.bf:99/tvbox/zhibo.php",
+    "http://api.mcqq.cn/tvbox/zhibo.php",
+    "http://39.101.135.137:8081/ls.txt"
     ]
 file_contents = []
 results = []
 for url in load_urls:
+    file_contents.append(f"//地址：{url}\n")
     response = requests.get(url, allow_redirects=True)
     if response.status_code == 200:
         # print(response.text)
@@ -20,6 +23,7 @@ for url in load_urls:
             # 你可以选择一个默认的编码，或者记录一个错误，或者采取其他措施
             content = response.content.decode('utf-8', errors='ignore')
         file_contents.append(content)
+        
 url_list = [line for line_str in file_contents for line in line_str.split('\n')]
 def text_list(list_str):
     if len(list_str) > 0:
