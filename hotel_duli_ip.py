7110f2a932a7bfa8b8710c9bb78e2a7c5375b773
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-04-08 21:36:31 +0800

    Update hotel_duli_ip.py

--- hotel_duli_ip.py
+++ hotel_duli_ip.py
@@ -37,7 +37,7 @@ def is_odd_or_even(number):
 
     
 sorted_list = [
-  "182.123.72.250:8090"
+  "61.52.158.49:808"
 ]
 
 
@@ -171,7 +171,7 @@ with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
 infoList = set(infoList)  # 去重得到唯一的URL列表
 infoList = sorted(infoList)
 
-with open("unicom_182_123_72.txt", 'w', encoding='utf-8') as file:
+with open("unicom_21.txt", 'w', encoding='utf-8') as file:
     for info in infoList:
         file.write(info + "\n")
         print(info)
