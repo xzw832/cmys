c87333b03dd5fc27654d65e20e811261433279ac
xzw832 <161630643+xzw832@users.noreply.github.com>
2024-04-10 04:06:48 +0800

    Update ip_qianxin.py

--- ip_qianxin.py
+++ ip_qianxin.py
@@ -1,3 +1,5 @@
+import subprocess
+import concurrent.futures
 import os
 import re
 import base64
@@ -153,7 +155,78 @@ for i in range(1, spider_cfg['page'] + 1):
             continue
         time.sleep(5)
 flattened_list = set(flattened_list)
-flattened_list = sorted(flattened_list)
+results = []
+for line in flattened_list:
+    try:
+        line = line.strip()
+        count = line.count(',')
+        if count == 1:
+            if line:
+                channel_name, channel_url = line.split(',')
+                result = channel_name, channel_url, "0.001 MB/s"
+                results.append(result)
+flattened_list = set(results)
+# 测试分辨率
+def check_video_source_with_ffmpeg(url):
+    cmd = ['ffprobe', '-v', 'error', '-select_streams', 'v:0',
+           '-show_entries', 'stream=codec_name,width,height,r_frame_rate', '-of',
+           'default=noprint_wrappers=1:nokey=1', url]
+    
+    try:
+        result = subprocess.run(cmd, capture_output=True, check=True, timeout=20, text=True)
+        output = result.stdout
+        # print(output)
+        # 使用正则表达式匹配并提取信息
+        pattern = r'^(h264)\s+(\d+)\s+(\d+)\s+(\d+/\d+)?$'
+        matches = re.findall(pattern, output, re.MULTILINE)
+        
+        if matches:
+            codec_name, width, height, r_frame_rate = matches[0]
+            return codec_name, int(width), int(height), int(eval(r_frame_rate))
+        else:
+            raise ValueError("No valid matches found in ffprobe output.")
+    
+    except subprocess.CalledProcessError as e:
+        return f"ffprobe command failed with error: {e}"
+    except subprocess.TimeoutExpired:
+        return "ffprobe command timed out."
+    except Exception as e:
+        return f"An unexpected error occurred: {e}"
+
+def process_video(video_url):
+    channel_name, channel_url, speed = video_url
+    try:
+        codec_name, width, height, r_frame_rate = check_video_source_with_ffmpeg(channel_url)
+        return (codec_name, width, height, r_frame_rate)
+    except ValueError as e:
+        print(f"Error parsing ffprobe output for {video_url}: {e}")
+        return (None, None, None, None)
+    except Exception as e:
+        print(f"An error occurred for {video_url}: {e}")
+        return (None, None, None, None)
+
+# 最大线程数
+max_workers = 50
+video_urls = set(results)
+results = []
+# 使用ThreadPoolExecutor创建线程池
+with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+    # 提交任务到线程池
+    future_to_url = {executor.submit(process_video, url): url for url in video_urls}
+    for future in concurrent.futures.as_completed(future_to_url):
+        url = future_to_url[future]
+        try:
+            # 获取任务返回的结果（分辨率和码率）
+            resolution_and_bitrate = future.result()
+            # 处理或记录结果
+            print(f"Results for {url}: {resolution_and_bitrate}")
+            codec_name, width, height, r_frame_rate = resolution_and_bitrate
+            if codec_name == 'h264' and width >= 720 and r_frame_rate < 50:
+                results.append(url)
+        except Exception as exc:
+            print(f'{url} generated an exception: {exc}')
+
+flattened_list = sorted(results)
 with open("ip_qianxin.txt", 'w', encoding='utf-8') as file:
     for line in flattened_list:
         try:
